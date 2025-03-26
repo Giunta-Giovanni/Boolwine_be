87c7e6f06f6047f58f92110bace4e0176e2597e6
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-26 11:44:28 +0100

    [feat] add comment

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -84,14 +84,12 @@ function post(req, res) {
 
     // check if cart is empty
     if (!cart || cart.length === 0) {
-        // cart is empty
-        console.error('cart is empty:', err);
+        console.error('cart is empty');
         return res.status(400).json({ error: 'cart is empty' });
     }
 
-    // create query: send order
-    const sendOrderSql = ` 
-        
+    // create query: insert order
+    const sendOrderSql = `
         INSERT INTO orders (
             total_price, 
             full_name, 
@@ -101,66 +99,57 @@ function post(req, res) {
             zip_code, 
             country
         )
-        VALUES ( 
-            ?, 
-            ?, 
-            ?, 
-            ?, 
-            ?, 
-            ?, 
-            ? 
-        );
+        VALUES (?, ?, ?, ?, ?, ?, ?);
     `;
 
-    // use query
-    connection.query(
-        sendOrderSql,
-        [totalPrice, fullName, email, phoneNumber, address, zipCode, country],
-        (err, result) => {
+    // execute query: insert order
+    connection.query(sendOrderSql, [totalPrice, fullName, email, phoneNumber, address, zipCode, country], (err, result) => {
+        if (err) {
+            console.error('database query failed:', err);
+            return res.status(500).json({ error: 'database query failed' });
+        }
+
+        // save order ID
+        const orderId = result.insertId;
+
+        // prepare data for order_details table
+        const values = cart.map((item) => [orderId, item.wine_id, item.quantity]);
+
+        // create query: insert order details
+        const addDetailsOrderSql = `
+            INSERT INTO order_details (order_id, wine_id, quantity) VALUES ?;
+        `;
+
+        // execute query: insert order details
+        connection.query(addDetailsOrderSql, [values], (err, result) => {
             if (err) {
-                // console err
-                console.error('database query failed:', err);
-                // response err
-                return res.status(500).json({ error: 'database query failed' });
-            };
+                console.error('failed to insert order details:', err);
+                return res.status(500).json({ error: 'failed to insert order details' });
+            }
 
-            // Salva l'ID dell'ordine appena creato
-            const orderId = result.insertId;
-            console.log(orderId);
+            // create query: update stock
+            const updateStockSql = `
+                UPDATE wines 
+                JOIN order_details ON wines.id = order_details.wine_id
+                SET wines.quantity_in_stock = wines.quantity_in_stock - order_details.quantity
+                WHERE order_details.order_id = ?;
+            `;
 
-            // Prepara i dati per la tabella order_details
-            const values = cart.map((item) => [orderId, item.wine_id, item.quantity])
-            // -- Inserisce più vini nel carrello (ordine dettagliato)
-            const addDetailsOrderSql = `
-            INSERT INTO order_details(
-                order_id, 
-                wine_id, 
-                quantity
-            )
-            VALUES ?
-            `
+            // execute query: update stock
+            connection.query(updateStockSql, [orderId], (err, result) => {
+                if (err) {
+                    console.error('failed to update stock:', err);
+                    return res.status(500).json({ error: 'failed to update stock' });
+                }
 
-            const updateStockSql =
-                `
-            UPDATE wines 
-            JOIN order_details ON wines.id = order_details.wine_id
-            SET wines.quantity_in_stock = wines.quantity_in_stock - order_details.quantity
-            WHERE order_details.order_id = ?;
-            `
-
-            connection.query(addDetailsOrderSql, [values], (err, result) => {
-                if (err) return console.error(err);
-
-                connection.query(updateStockSql, [orderId], (err, result) => {
-                    if (err) return console.error(err);
-                    console.log("Stock aggiornato con successo.");
-                    res.status(201).json({ message: 'stock aggiornato con successo' })
-                });
+                // response: success message
+                console.log('order created & stock updated');
+                res.status(201).json({ order: `order number ${orderId} created`, stock: `stock updated` });
             });
-
-
         });
-};
+    });
+}
+
 
 
 
@@ -176,9 +165,33 @@ module.exports = { index, show, post, modify };
 
 
 
+//     // save quantity from req.body
+//     const { quantity } = req.body;
 
-// -- Aggiorna le quantità in stock per tutti i vini ordinati
-// UPDATE wines 
-// JOIN order_details ON wines.id = order_details.wine_id
-// SET wines.quantity_in_stock = wines.quantity_in_stock - order_details.quantity
-// WHERE order_details.order_id = @order_id;
+
+
+
+
+//     // use query
+//     connection.query(checkWineSql, [id], (err, checkWineResult) => {
+//         if (err) {
+//             // console err
+//             console.error('database query failed:', err);
+//             // response err
+//             return res.status(500).json({ error: 'database query failed' });
+//         }
+
+//         if (checkWineResult.length === 0) {
+//             // response err if the wine does not exist
+//             return res.status(404).json({ error: 'wine not found' });
+//         }
+
+//         // save result
+//         const wine = checkWineResult[0];
+//         const currentQuantity = wine.quantity_in_stock;
+
+//         // check if the requested quantity is available
+//         if (quantity > currentQuantity) {
+//             // response err if not enough stock is available
+//             return res.status(400).json({ error: 'not enough stock available' });
+//         }
