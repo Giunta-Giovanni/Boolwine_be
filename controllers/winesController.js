dee194f911c2fd061e8c48ca8595088771fc2197
GiuntaGiovanni <jhonniest741@gmail.com>
2025-03-27 11:41:17 +0100

    [feat] filter in index

--- controllers/winesController.js
+++ controllers/winesController.js
@@ -11,7 +11,8 @@ function index(req, res) {
         types.name AS type
         FROM wines
         JOIN types ON types.id = wines.type_id
-    `
+    `;
+
     // use query
     connection.query(winesSql, (err, winesResults) => {
         if (err) {
@@ -19,12 +20,12 @@ function index(req, res) {
             console.error('database query failed:', err);
             // response err
             return res.status(500).json({ error: 'database query failed' });
-        }
+        };
 
         if (winesResults.length === 0) {
             // response err
             return res.status(404).json({ error: 'no wines found' });
-        }
+        };
 
         // update path image
         const wines = winesResults.map(wine => {
@@ -35,10 +36,41 @@ function index(req, res) {
             };
         });
 
+        // create a variable to store filtered wines
+        let filteredWines = wines;
+
+        // if query parameter "type" is present
+        if (req.query.type) {
+            // set all types in lower case
+            const typeQuery = req.query.type.toLowerCase();
+            // create a new array with requested type
+            filteredWines = wines.filter(wine => wine.type.toLowerCase().includes(typeQuery));
+        };
+
+        // if query parameter "search" is present
+        if (req.query.search) {
+            // set all search in lower case
+            const searchQuery = req.query.search.toLowerCase();
+            // create a new array with requested name or type
+            filteredWines = filteredWines.filter(wine => wine.name.toLowerCase().includes(searchQuery) || wine.type.toLowerCase().includes(searchQuery));
+        };
+
+        // if filteredWines is empty
+        if (filteredWines.length === 0) {
+            // response err
+            return res.status(404).json({ error: 'no filtered wines found' });
+        };
+
         // response: all wines
-        res.json(wines);
+        res.json(filteredWines);
     });
 }
+
+function indexBestWines(req, res) {
+
+}
+
+
 
 // SHOW FUNCTION
 function show(req, res) {
@@ -80,67 +112,6 @@ function show(req, res) {
     });
 }
 
-// MODIFY FUNCTION
-// function modify(req, res) {
-
-//     // save id from req.params
-//     const { id } = req.params;
-//     // save quantity from req.body
-//     const { quantity } = req.body;
-
-//     // create query: check if the wine exists in the database
-//     const checkWineSql = `
-//         SELECT name, quantity_in_stock
-//         FROM wines
-//         WHERE id = ?
-//     `;
-
-//     // use query
-//     connection.query(checkWineSql, [id], (err, checkWineResult) => {
-//         if (err) {
-//             // console err
-//             console.error('database query failed:', err);
-//             // response err
-//             return res.status(500).json({ error: 'database query failed' });
-//         }
-
-//         if (checkWineResult.length === 0) {
-//             // response err if the wine does not exist
-//             return res.status(404).json({ error: 'wine not found' });
-//         }
-
-//         // save result
-//         const wine = checkWineResult[0];
-//         const currentQuantity = wine.quantity_in_stock;
-
-//         // check if the requested quantity is available
-//         if (quantity > currentQuantity) {
-//             // response err if not enough stock is available
-//             return res.status(400).json({ error: 'not enough stock available' });
-//         }
-
-//         // create query: update the quantity in stock
-//         const boughtQuantitySql = `
-//             UPDATE wines
-//             SET quantity_in_stock = quantity_in_stock - ?
-//             WHERE id = ?
-//         `;
-
-//         // use query
-//         connection.query(boughtQuantitySql, [quantity, id], (err, result) => {
-//             if (err) {
-//                 // console err
-//                 console.error('database query failed:', err);
-//                 // response err
-//                 return res.status(500).json({ error: 'database query failed' });
-//             }
-
-//             // response: success message with purchase details
-//             res.status(200).json({ message: `${quantity} bottle(s) of ${wine.name} has been bought` });
-//         });
-//     });
-// }
-
 
 // EXPORT
-module.exports = { index, show, };
+module.exports = { index, indexBestWines, show, };
