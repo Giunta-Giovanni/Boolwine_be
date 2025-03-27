b5b47a12e02147591c173b171714eb8e7ed6c780
penso996 <penso.andrea@outlook.it>
2025-03-27 21:51:24 +0100

    order and lightened code

--- routes/winesRouter.js
+++ routes/winesRouter.js
@@ -5,17 +5,18 @@ const router = express.Router();
 // import controllers
 const winesController = require('../controllers/winesController');
 // controllers destructuring
-const { index, indexBestWines, indexLimitedStock, show } = winesController
+const { index, show, indexLimitedStock, indexBestWines } = winesController
 
 // Crud Operations
 // index
 router.get('/', index);
-// index: best wines
-router.get('/best_wines', indexBestWines);
-// index: best seller
-router.get('/best_seller', indexLimitedStock);
 // show
 router.get('/:id', show);
+// index limited stock
+router.get('/limited_stock', indexLimitedStock);
 
+// TODO
+// index: best wines
+router.get('/best_wines', indexBestWines);
 
 module.exports = router;
